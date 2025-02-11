b31fb5ca528562cea21cf0846e6b625fd28995f8
@hexlet/cli <support@hexlet.io>
2025-02-11 19:04:12 +0300

    submit java-web-ru/template

--- java-web-ru/template/src/main/java/exercise/App.java
+++ java-web-ru/template/src/main/java/exercise/App.java
@@ -22,21 +22,25 @@ public final class App {
         });
 
         // BEGIN
-        app.get("/users", ctx -> {
-            List<User> users = USERS;
-            UsersPage usersPage = new UsersPage(USERS);
-            ctx.render("users/index.jte", model("usersPage", usersPage));
+        app.get("/users/{id}", ctx -> {
+            var id = ctx.pathParamAsClass("id", Long.class).get();
+            User user = USERS.stream()
+                    .filter(u -> id.equals(u.getId()))
+                    .findFirst()
+                    .orElse(null);
+
+            if (user == null) {
+                throw new NotFoundResponse("User not found");
+            }
+
+            var page = new UserPage(user);
+            ctx.render("users/show.jte", model("page", page));
         });
 
-        app.get("/users/{id}", ctx -> {
-            int userId = Integer.parseInt(ctx.pathParam("id"));
-            User user = USERS.stream()
-                    .filter(u -> u.getId() == userId)
-                    .findFirst()
-                    .orElseThrow(() -> new NotFoundResponse("User not found"));
+        app.get("/users", ctx -> {
+            var page = new UsersPage(USERS);
+            ctx.render("users/index.jte", model("page", page));
 
-            UserPage userPage = new UserPage(user);
-            ctx.render("users/show.jte", model("userPage", userPage));
         });
         // END
 
