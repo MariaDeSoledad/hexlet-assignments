bb2fc9c983d635ee17026fe628a90ca16096157e
@hexlet/cli <support@hexlet.io>
2025-03-11 21:46:51 +0300

    submit java-spring-boot-ru/service-layer

--- java-spring-boot-ru/service-layer/src/main/java/exercise/service/BookService.java
+++ java-spring-boot-ru/service-layer/src/main/java/exercise/service/BookService.java
@@ -5,8 +5,11 @@ import exercise.dto.BookDTO;
 import exercise.dto.BookUpdateDTO;
 import exercise.exception.ResourceNotFoundException;
 import exercise.mapper.BookMapper;
+import exercise.model.Book;
 import exercise.repository.BookRepository;
+import exercise.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,6 +23,9 @@ public class BookService {
     @Autowired
     private BookMapper bookMapper;
 
+    @Autowired
+    private AuthorRepository authorRepository;
+
     public List<BookDTO> getAll() {
         return bookRepository.findAll()
                 .stream()
@@ -28,8 +34,17 @@ public class BookService {
     }
 
     public BookDTO create(BookCreateDTO bookData) {
+        // Проверка существования автора с данным ID
+        if (!authorRepository.existsById(bookData.getAuthorId())) {
+            throw new ResourceNotFoundException("Author with id = " + bookData.getAuthorId() + " not found");
+        }
+
         var book = bookMapper.map(bookData);
-        bookRepository.save(book);
+        try {
+            bookRepository.save(book);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityViolationException("Could not save book: " + e.getMessage());
+        }
         return bookMapper.map(book);
     }
 
